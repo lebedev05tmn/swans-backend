a6484f6d818762f37894214211a4ab9737680f34
kosobutski <kailyukhin@gmail.com>
2024-12-23 02:09:00 +0300

    webp, get-request bug fixed

--- src/core-media/controllers/upload-media.ts
+++ src/core-media/controllers/upload-media.ts
@@ -1,32 +1,30 @@
 import { Request, Response } from 'express';
-import path from 'path';
-import * as fs from 'node:fs';
 import { v4 as uuid } from 'uuid';
 import { UploadedFile } from 'express-fileupload';
 import { minioClient } from '../minio-client';
 import { HTTP_STATUSES } from '../../shared/utils';
 import { bucketName } from '../../shared/config';
+import sharp from 'sharp';
 
 export const uploadMedia = async (req: Request, res: Response) => {
     if (req.files) {
         const file = req.files.file as UploadedFile;
-        const extname = path.extname(file.name);
         const id = uuid();
-        const fileStream = fs.createReadStream(file.tempFilePath);
+        const webpFileName = `${id}.webp`;
 
-        if (file) {
-            await minioClient
-                .putObject(bucketName, `${id}${extname}`, fileStream)
-                .then(() => {
-                    return res.status(HTTP_STATUSES.CREATED_201).send(id);
-                })
-                .catch((err) => {
-                    return res.status(HTTP_STATUSES.NOT_FOUND_404).send(err);
-                });
+        try {
+            const webpBuffer = await sharp(file.tempFilePath)
+                .webp({ quality: 80 })
+                .toBuffer();
+
+            await minioClient.putObject(bucketName, webpFileName, webpBuffer);
+            return res.status(HTTP_STATUSES.CREATED_201).send(id);
+        } catch (error) {
+            return res.status(500).send(`Failed to upload media: ${error}`);
         }
     } else {
         return res
             .status(HTTP_STATUSES.BAD_REQUEST_400)
-            .send(`No files uploaded.`);
+            .send('No files uploaded.');
     }
 };
