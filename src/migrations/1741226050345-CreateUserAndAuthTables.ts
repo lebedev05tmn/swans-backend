7c5d0c0e3f88a028a8b473d6711494c3f58886c7
4318 Byrsan Vadim Andreevich <vadim.byrsan@mail.ru>
2025-03-07 02:10:54 +0300

    Редактирование файла миграции на typeORM

--- src/migrations/1741226050345-CreateUserAndAuthTables.ts
+++ src/migrations/1741226050345-CreateUserAndAuthTables.ts
@@ -1,31 +1,78 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
+import {
+    MigrationInterface,
+    QueryRunner,
+    Table,
+    TableForeignKey,
+} from 'typeorm';
 
 export class CreateUserAndAuthTables1741226050345
     implements MigrationInterface
 {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
-            CREATE TABLE "user" (
-                "user_id" character varying NOT NULL,
-                "refresh_token" character varying,
-                CONSTRAINT "PK_user_id" PRIMARY KEY ("user_id")
-            )
-        `);
+        await queryRunner.createTable(
+            new Table({
+                name: 'user',
+                columns: [
+                    {
+                        name: 'user_id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        isNullable: false,
+                    },
+                    {
+                        name: 'refresh_token',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                ],
+            }),
+            true,
+        );
 
-        await queryRunner.query(`
-            CREATE TABLE "auth" (
-                "auth_id" SERIAL NOT NULL,
-                "service_user_id" character varying NOT NULL,
-                "service_name" text NOT NULL,
-                "userUserId" character varying,
-                CONSTRAINT "PK_auth_id" PRIMARY KEY ("auth_id"),
-                CONSTRAINT "FK_auth_userUserId" FOREIGN KEY ("userUserId") REFERENCES "user"("user_id") ON DELETE CASCADE
-            )
-        `);
+        await queryRunner.createTable(
+            new Table({
+                name: 'auth',
+                columns: [
+                    {
+                        name: 'auth_id',
+                        type: 'int',
+                        isPrimary: true,
+                        isGenerated: true,
+                        generationStrategy: 'increment',
+                    },
+                    {
+                        name: 'service_user_id',
+                        type: 'varchar',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'service_name',
+                        type: 'text',
+                        isNullable: false,
+                    },
+                    {
+                        name: 'userUserId',
+                        type: 'varchar',
+                        isNullable: true,
+                    },
+                ],
+            }),
+            true,
+        );
+
+        await queryRunner.createForeignKey(
+            'auth',
+            new TableForeignKey({
+                columnNames: ['userUserId'],
+                referencedColumnNames: ['user_id'],
+                referencedTableName: 'user',
+                onDelete: 'CASCADE',
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "auth"`);
-        await queryRunner.query(`DROP TABLE "user"`);
+        await queryRunner.dropTable('auth');
+        await queryRunner.dropTable('user');
     }
 }
