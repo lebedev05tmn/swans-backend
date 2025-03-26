dd521428c3b2f38d4a892290ee41318592370869
Leezzzzzzyy <vadim.byrsan@mail.ru>
2025-03-26 17:19:01 +0300

    Пример использования маппинга JWT в getUserAuthData

---
+++ src/core-auth/utils/getUserId.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { HTTP_STATUSES } from '../../../src/shared/utils';
+import jwt from 'jsonwebtoken';
+import jwtConfig from '../../shared/config/JWTConfig';
+
+export default (req: Request, res: Response): string => {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer '))
+        throw new Error('Missing or invalid Authorization header!');
+
+    const token = authHeader.split(' ')[1];
+
+    let decodedToken: any;
+    try {
+        decodedToken = jwt.verify(token, jwtConfig.secret);
+    } catch (error) {
+        throw new Error('Invalid or expired token!');
+    }
+
+    const user_id = decodedToken.userId;
+
+    if (!user_id) throw new Error('User not Found in Token Payload!');
+
+    return user_id;
+};
